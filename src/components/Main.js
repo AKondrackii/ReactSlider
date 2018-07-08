e3869944218e90876b664b7761a83f030c402757
= <=>
2018-07-08 15:06:37 +0300

    Add slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Slider from './Slider';
+
 const Repair = styled.div`
 	color: #fff;
 	font-size: 40px;
@@ -55,7 +57,7 @@ class Main extends React.Component {
 						</CallButton>
 					</Col>
 					<Col lg={6} lgOffset={1}>
-						456
+						<Slider />
 					</Col>
 				</Row>
 			)
